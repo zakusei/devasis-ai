fef95ca4d00a7e74a13bb6cb86d49d558a0ce742
Zakusei <zakusei.dev@gmail.com>
2023-11-30 22:37:41 +0800

    initial commit for roadmap page

---
+++ app/roadmap/page.jsx
@@ -0,0 +1,12 @@
+import Container from "@/components/layouts/Container";
+import Navbar from "@/components/layouts/Navbar";
+
+const Roadmap = () => {
+  return (
+    <Container>
+      <Navbar />
+    </Container>
+  );
+};
+
+export default Roadmap;
