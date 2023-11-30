774590148de297e894c73d9a64e2bc34edbf5605
Zakusei <zakusei.dev@gmail.com>
2023-11-30 16:52:28 +0800

    feat: add home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,30 @@
-"use client";
+import FlexContainer from "@/components/layouts/Container";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { TiFolderAdd, TiFolder } from "react-icons/ti";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <FlexContainer>
+      <section className="grid w-full max-w-xs gap-2 rounded-md">
+        <h1 className="mb-6 w-full text-center text-3xl font-semibold">
+          D E V A S I S
+        </h1>
+        <Link
+          href="/create"
+          className={cn(buttonVariants({ variant: "outline" }))}
+        >
+          <TiFolderAdd className="mr-2 h-4 w-4" />
+          New Project
+        </Link>
+        <Link href="#" className={cn(buttonVariants({ variant: "default" }))}>
+          <TiFolder className="mr-2 h-4 w-4" />
+          Open Project
+        </Link>
+      </section>
+    </FlexContainer>
+  );
 };
 
 export default Home;
