f3d957201823a045c51e0993991f846fbe573669
Zakusei <zakusei.dev@gmail.com>
2023-12-01 23:43:16 +0800

    refactor(navbar): style navbar to full rounded

--- components/layouts/Navbar.jsx
+++ components/layouts/Navbar.jsx
@@ -6,20 +6,25 @@ import { usePathname } from "next/navigation";
 const Navbar = () => {
   const path = usePathname();
   return (
-    <nav className="fixed inset-x-0 bottom-0 flex h-16 items-center border-t">
-      <ul className="flex w-full justify-evenly">
-        {ROUTES.map(({ href, icon }, index) => (
-          <li key={index}>
-            <Link
-              href={href}
-              className={`text-${
-                href != path && "muted-"
-              }foreground hover:text-foreground`}
-            >
-              {icon}
-            </Link>
-          </li>
-        ))}
+    <nav className="fixed inset-x-6 bottom-6 h-16 rounded-full border bg-background shadow-lg">
+      <ul className="grid h-full w-full grid-cols-3 place-content-center rounded-full">
+        {ROUTES.map(({ href, icon }, index) => {
+          return (
+            <>
+              <li key={index} className={`flex cursor-pointer justify-center`}>
+                <Link
+                  href={href}
+                  className={`text-muted-foreground ${
+                    href == path &&
+                    "animate-pulse text-foreground ease-in-out dark:text-white"
+                  } hover:text-foreground`}
+                >
+                  {icon}
+                </Link>
+              </li>
+            </>
+          );
+        })}
       </ul>
     </nav>
   );
