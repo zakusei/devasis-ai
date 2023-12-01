cdbc2334cf85b39d6c08a690ea80ba106190253d
Zakusei <zakusei.dev@gmail.com>
2023-12-02 01:16:55 +0800

    refactor: fix deployment bug issue

--- components/layouts/Footer.jsx
+++ components/layouts/Footer.jsx
@@ -3,7 +3,7 @@ import GDSC from "@/public/assets/img/gdsc.svg";
 
 const Footer = () => {
   return (
-    <footer className="absolute inset-x-0 bottom-0 flex flex-col place-items-center gap-4 pb-12 text-center ">
+    <footer className="absolute inset-x-0 bottom-0 flex flex-col place-items-center gap-4 bg-transparent pb-12 text-center">
       <Image width={32} height={32} src={GDSC.src} />
       <span className="text-xs font-medium">Google Developer Student Club</span>
     </footer>
