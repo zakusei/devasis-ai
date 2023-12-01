4eef34971e3eab60e0cf71990d5684ac529834e2
Zakusei <zakusei.dev@gmail.com>
2023-12-01 23:42:43 +0800

    feat(tasks-page): initial commit for tasks page

---
+++ app/tasks/page.jsx
@@ -0,0 +1,12 @@
+import Container from "@/components/layouts/Container";
+import Navbar from "@/components/layouts/Navbar";
+
+const Tasks = () => {
+  return (
+    <Container>
+      <Navbar />
+    </Container>
+  );
+};
+
+export default Tasks;
