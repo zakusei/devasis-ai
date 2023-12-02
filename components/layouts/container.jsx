f250073eb113d0e8469cf864c8a36406ee9f3152
Zakusei <zakusei.dev@gmail.com>
2023-12-02 23:36:57 +0800

    refactor(container): remove padding

--- components/layouts/container.jsx
+++ components/layouts/container.jsx
@@ -4,7 +4,7 @@ const Container = ({ children, className }) => {
   return (
     <div
       className={cn(
-        "mx-auto flex h-screen w-full flex-1 flex-col bg-background p-6 md:container md:px-96",
+        "mx-auto flex h-screen w-full flex-1 flex-col bg-background p-6 md:container ",
         className,
       )}
     >
