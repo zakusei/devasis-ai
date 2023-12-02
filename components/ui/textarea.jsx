73521c8b8ef0a3a6cf01d4ddeb0a749ccc183446
Zakusei <zakusei.dev@gmail.com>
2023-12-02 23:38:27 +0800

    refactor(textarea): remove outline style on focus

--- components/ui/textarea.jsx
+++ components/ui/textarea.jsx
@@ -6,7 +6,7 @@ const Textarea = React.forwardRef(({ className, ...props }, ref) => {
   return (
     <textarea
       className={cn(
-        "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-xs ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "flex w-full rounded-md border border-input bg-background px-4 py-4 text-xs placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         className,
       )}
       ref={ref}
