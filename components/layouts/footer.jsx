5a85c3e28f3fd843d75c075a9f23cd014e4e055f
Zakusei <zakusei.dev@gmail.com>
2023-12-02 23:37:28 +0800

    refactor(footer): import cn for classname

--- components/layouts/footer.jsx
+++ components/layouts/footer.jsx
@@ -1,9 +1,15 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import GDSC from "@/public/assets/img/gdsc.svg";
 
-const Footer = () => {
+const Footer = ({ className }) => {
   return (
-    <footer className="absolute inset-x-0 bottom-0 flex flex-col place-items-center gap-4 bg-transparent pb-12 text-center">
+    <footer
+      className={cn(
+        "absolute inset-x-0 bottom-0 flex flex-col place-items-center gap-4 bg-transparent pb-12 text-center",
+        className,
+      )}
+    >
       <Image width={32} height={32} src={GDSC.src} />
       <span className="text-xs font-medium">Google Developer Student Club</span>
     </footer>
